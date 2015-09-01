774f1b0bccc0dbb653cc1ad215775e8dca7b07b3
Ashish Sharma <a-sharma@outlook.com>
2015-09-01 16:20:28 -0400

    integrated with xignite

--- index.js
+++ index.js
@@ -1,12 +1,38 @@
 $(function(){
+	var securityFields = ["Outcome","Last", "LastSize", "Open", "High", "Low", "Close", "Volume", "Message", "Delay", "Date", "Time", "UTCOffset", ];
+	
+	$.get('/IdentifierTypes')
+	.done(function(response){
+		var identifiersDropdown = $('#securityIdentifiersDropdown');
+		identifiersDropdown.empty();
+		for(var i = 0; i < response.length; i++)
+		{
+			identifiersDropdown.append(new Option(response[i].Name, response[i].Name, false, false));
+		}
+	})
+	.fail(function(error){
+		console.log(error);
+    	alert('Error getting security identifiers. Status = ' + error.status + ' ' + error.responseText);
+	})
 	$('#getSecurityButton').click(function(){
-		$.get('/Security')
-		.done(function(response){
-			$('#securityDescriptionLabel').text(response);
-		})
-		.fail(function(xhr){
-			console.log(xhr);
-    		alert(xhr.status + ' ' + xhr.responseText);
-		})
+		$('#securityDescriptionLabel').text('Loading.......');
+		var securitiesArray = $('#symbolTextBox').val().split(';');
+		for(var i = 0; i < securitiesArray.length; i++)
+		{
+			$.post('/Securities', { IdentifierType : $('#securityIdentifiersDropdown').val(), Identifier : securitiesArray[i]})
+				.done(function(response){
+				//$('#securityDescriptionLabel').text("");
+					
+				// $.each(response, function(key, value) {
+   				// 	$('#displayDiv').append('<p>' + key + '-->' + value + '</p>')
+				// });
+				$('#securityDescriptionLabel').text(JSON.stringify(response));
+			})
+			.fail(function(error){
+				console.log(error);
+    			alert('Error getting security Definition. Status = ' + error.status + ' ' + error.responseText);
+				$('#securityDescriptionLabel').text("");
+			})
+		}
 	});
 });
--- main.js
+++ main.js
@@ -3,16 +3,58 @@ var app = express();
 app.use(express.static(__dirname));
 var bodyParser = require('body-parser');
 var request = require('request');
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+// parse application/json
+app.use(bodyParser.json());
+
+var securityIdentifiers = [{Name :"Symbol"}, {Name :"CIK"}, {Name : "CUSIP"}, {Name : "ISIN"}, {Name : "VALOREN"}, {Name : "SEDOL"}];
+
 
 app.get('/', function(req, res){
 	res.sendFile('index.html');
 });
 
+app.get('/IdentifierTypes', function(req, res){
+	res.json(securityIdentifiers);
+});
 
-app.get('/Security', function(req, res){
-	res.send('Hold your horses, i am still trying to figure out integration with 3rd party market data providers');
+app.post('/Securities', function(req, res){
+	
+	var identifierType = req.body.IdentifierType;
+	var identifier = req.body.Identifier;
+	console.log(req.body);
+	var options = {
+  		url: createXigniteUrl(identifierType, identifier),
+  		method: 'GET',
+    }
+	console.log('Url: ' + options.url);
+	request.get(options, function(error, response, body){
+		try {
+			console.log(body);
+			if(!error && response.statusCode == 200)
+			{
+				var security = JSON.parse(body);
+				if(security.Outcome != "Success")
+					throw body;
+				res.json(security);
+			}
+			else
+				throw error;
+		} catch (error) {
+			res.status(400).send(error);
+		}
+	});
 });
 
 app.listen(3000, function(){
 	console.log('Server started at ' + 3000);
 });
+
+
+function createXigniteUrl(identifier, ticker)
+{
+	var template = 'http://globalquotes.xignite.com/v3/xGlobalQuotes.json/GetGlobalDelayedQuote?_token=BE130EE38FD84F0B93917BBF0B6F437F&Identifier=#{identifier}&IdentifierType=#{identifierType}';
+	template = template.replace('#{identifier}', ticker);
+	return template.replace('#{identifierType}', identifier);
+}
